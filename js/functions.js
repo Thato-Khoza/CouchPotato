c7bf53b384746ab03b40c4f5ae9c3d8b57c54cef
Thato-Khoza <91176332+Thato-Khoza@users.noreply.github.com>
2021-10-06 00:44:15 +0200

    Log in & Sign up pages created
    This page is reactive and the  functionality in the slider for the desktop and phone screen is active.

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,32 @@ $(document).ready(function() {
 
     
 });
+
+/*sighUp slider*/
+$(function(){
+
+    $(".btn").on("click", function(){
+        $(".panel").toggleClass("panel-slide");
+    });
+
+ });
+
+
+ $(function(){
+
+    $(".leftBtn").on("click" , function(){
+        $(".left").removeClass( "Appear" ).addClass( "disAppear") 
+        $(".right").removeClass( "disAppear" ).addClass( "Appear")
+
+    });
+    
+});
+
+$(function(){
+
+    $(".rightBtn").on("click" , function(){
+        $(".right").removeClass( "Appear" ).addClass( "disAppear") 
+        $(".left").removeClass( "disAppear" ).addClass( "Appear") ;
+
+    });
+});
