1d142ef378743c0502ebbf98bf468a98b68dbbd2
Alexa Pettitt <21100290@virtualwindow.co.za>
2021-10-13 20:43:52 +0200

    Dynamically loaded API content and did hover styling
    Here I dynamically loaded the API content and made the hover work - I also adjusted minor styling details.

--- js/libraryFunction.js
+++ js/libraryFunction.js
@@ -3,14 +3,17 @@ $(document).ready(function() {
     console.log("jQuery added & ready");
 
     /*API*/
-const URL='https://api.themoviedb.org/3/movie/550?api_key=7e01193d3f7015b3d6f900efd5b545c2';
+const URL='https://api.themoviedb.org/3/movie/popular?api_key=7e01193d3f7015b3d6f900efd5b545c2';
 
 $.getJSON(URL,function(result){
     console.log(result);
 
+    console.log(result.results.length);
 
     //insert comedy row
-    for(i=0; i < 10; i++){
+    for(i=0; i < result.results.length; i++){
+
+        var genreCode = result.results[i].genre_ids[0];
         
 
         $(".row.movie-insert.comedy").append(
@@ -18,14 +21,14 @@ $.getJSON(URL,function(result){
 
                 "<div class = 'col-md-4 col-sm-6 col-12'>\
                 <div class='movieCard'>\
-                    <img class='movieImg' src=''/>\
+                    <img class='movieImg' src='https://image.tmdb.org/t/p/w500"+result.results[i].poster_path+"'/>\
                     <div class='overlayBlock'>\
                         <div class='divheart'>\
                             <img src='../img/other/heart_icon.svg'/>\
                         </div><!--divheart-->\
                         <div class='overlay'>\
                             <div class='hover-text'>\
-                                <h6>Movie Title</h6>\
+                                <h6>" + result.results[i].original_title +"</h6>\
                                 <h5>Rating</h5>\
                                 <div class='button1 movieButton'>Discover</div>\
                                 <div class='button2 movieButton'>Watch Later</div>\
@@ -34,10 +37,12 @@ $.getJSON(URL,function(result){
                     </div><!--overlayBlock-->\
                 </div><!--movieCard-->\
              </div><!--col-->"
-
+            
 
 
             )
+           
+
         }
         
         });
