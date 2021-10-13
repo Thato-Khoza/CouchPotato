f4d3d793b79ff6273488b2dd3456e55baf18df36
Thato-Khoza <91176332+Thato-Khoza@users.noreply.github.com>
2021-10-13 07:53:53 +0200

    Updated signup
    Added icons onto form

--- js/functions.js
+++ js/functions.js
@@ -6,33 +6,67 @@ $(document).ready(function() {
     
 });
 
-/*sighUp slider*/
-$(function(){
+const signingUp = document.getElementById('signing-up');
+    signingIn = document.getElementById('signing-in');
+    logIn = document.getElementById('log-in');
+    logUp = document.getElementById('log-up');
 
-    $(".btn").on("click", function(){
-        $(".panel").toggleClass("panel-slide");
-    });
+signingUp.addEventListener('click', ()=>{
 
- });
+    logIn.classList.remove('block');
+    logUp.classList.remove('none');
+
+    logIn.classList.add('none');
+    logUp.classList.add('block');
+});
+
+signingIn.addEventListener('click', ()=>{
+
+    logIn.classList.remove('none');
+    logUp.classList.remove('block');
+
+    logIn.classList.add('block');
+    logUp.classList.add('none');
+});
 
 
- $(function(){
 
-    $(".leftBtn").on("click" , function(){
-        $(".left").removeClass( "Appear" ).addClass( "disAppear") 
-        $(".right").removeClass( "disAppear" ).addClass( "Appear")
 
-    });
+
+
+
+
+
+
+
+
+
+//sighUp slider
+//$(function(){
+
+  //  $(".btn").on("click", function(){
+    //    $(".panel").toggleClass("panel-slide");
+    //});
+
+ //});
+
+
+ //$(function(){
+
+   // $(".leftBtn").on("click" , function(){
+     //   $(".left").removeClass( "Appear" ).addClass( "disAppear") 
+       // $(".right").removeClass( "disAppear" ).addClass( "Appear")
+
+    //});
     
-});
+//});
 
-$(function(){
+//$(function(){
 
-    $(".rightBtn").on("click" , function(){
-        $(".right").removeClass( "Appear" ).addClass( "disAppear") 
-        $(".left").removeClass( "disAppear" ).addClass( "Appear") ;
+  //  $(".rightBtn").on("click" , function(){
+   //     $(".right").removeClass( "Appear" ).addClass( "disAppear") 
+     //   $(".left").removeClass( "disAppear" ).addClass( "Appear") ;
 
-    });
-});
-
+    //});
+//}); 
 
