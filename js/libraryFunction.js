851ebe21808fc1b797ac02217899f552d3bb9936
Hanrie-Bester <21100266@virtualwindow.co.za>
2021-10-19 22:03:11 +0200

    Slider fixed
    I have fixed the slider errors of the movie library page and added the code to the main branch

--- js/libraryFunction.js
+++ js/libraryFunction.js
@@ -1,20 +1,20 @@
 // jQuery Code Here
 $(document).ready(function() {
     console.log("jQuery added & ready");
-
+ 
     /*API*/
 const URL='https://api.themoviedb.org/3/movie/550?api_key=7e01193d3f7015b3d6f900efd5b545c2';
-
+ 
 $.getJSON(URL,function(result){
     console.log(result);
-
-
+ 
+ 
     //insert comedy row
     for(i=0;i<result.length;i++){
         
-
+ 
             $('.row.movie-insert.comedy').append(
-
+ 
                 "<div class = 'col-md-4 col-sm-6 col-12'>\
                 <div class='movieCard'>\
                     <img class='movieImg' src=''/>\
@@ -33,19 +33,19 @@ $.getJSON(URL,function(result){
                     </div><!--overlayBlock-->\
                 </div><!--movieCard-->\
              </div><!--col-->"
-
-
-
+ 
+ 
+ 
             )
         
     }
-
+ 
     
   
 for(i=0; i < 10; i++){
-        $(".movie-insert").append(
-
-        "<div class='movieCard' id='movieCardSlide'>\
+        $(".carouselbox").append(
+ 
+        "<div class='movieCard'>\
                 <img class='movieImg' src=''/>\
                 <div class='overlayBlock'>\
                     <div class='divheart'>\
@@ -61,40 +61,49 @@ for(i=0; i < 10; i++){
                     </div>\
                 </div>\
         </div>"
-        )
-    } //for loop(movie card repetition)
+    )
+} //for loop(movie card repetition)
     
 //on click button slider code
-var span = document.getElementsByTagName('span');
-var div= document.getElementsByClassName('movieCard');
-var l = 0;
-span[1].onclick = () =>{
-    l++;
-    for(var i of div)
-    {
-        if(l==0){i.style.left = "0px";}
-        if(l==1){i.style.left = "-740px";}
-        if(l==2){i.style.left = "-1480px";}
-        if(l==3){i.style.left = "-2220px";}
-        if(l==4){i.style.left = "-2960px";}
-        if(l>4){l=4;}
-    }
-}
-span[0].onclick = () =>{
-    l--;
-    for(var i of div)
-    {
-        if(l==0){i.style.left = "0px";}
-        if(l==1){i.style.left = "-740px";}
-        if(l==2){i.style.left = "-1480px";}
-        if(l==3){i.style.left = "-2220px";}
-        if(l==4){i.style.left = "-2960px";}
-        if(l<0){l=0;}
-    }
-}
+ 
+// COMEDY SLIDE
+ $("#ComedyLeft").click(function () { 
+    $('.carouselbox.comedy').scrollLeft($('.carouselbox.comedy').scrollLeft() - 800);
+ });
+ 
+ $("#ComedyRight").click(function () { 
+    $('.carouselbox.comedy').scrollLeft($('.carouselbox.comedy').scrollLeft() + 800);
+ });
+// ACTION SLIDE
+ $("#ActionLeft").click(function () { 
+    $('.carouselbox.action').scrollLeft($('.carouselbox.action').scrollLeft() - 800);
+ });
+ 
+ $("#ActionRight").click(function () { 
+    $('.carouselbox.action').scrollLeft($('.carouselbox.action').scrollLeft() + 800);
+ });
+ // HORROR SLIDE
+ $("#HorrorLeft").click(function () { 
+    $('.carouselbox.horror').scrollLeft($('.carouselbox.horror').scrollLeft() - 800);
+ });
+ 
+ $("#HorrorRight").click(function () { 
+    $('.carouselbox.horror').scrollLeft($('.carouselbox.horror').scrollLeft() + 800);
+ });
+ // ROMANCE SLIDE
+ $("#RomanceLeft").click(function () { 
+    $('.carouselbox.romance').scrollLeft($('.carouselbox.romance').scrollLeft() - 800);
+ });
+ 
+ $("#RomanceRight").click(function () { 
+    $('.carouselbox.romance').scrollLeft($('.carouselbox.romance').scrollLeft() + 800);
+ });
+ 
 });     
 });
-
-
-
+ 
+ 
+ 
+ 
+ 
 
