d828e8b53bfef706cfa2a1d00dd8cc8aee619179
Alexa Pettitt <21100290@virtualwindow.co.za>
2021-10-26 21:47:30 +0200

    Added similar movies and slider
    Here I added a slider containing similar movies to the individual movie page.

--- js/individualMovie.js
+++ js/individualMovie.js
@@ -99,11 +99,76 @@ $(document).ready(function() {
        )
 
         }
-       
+
+        //similar movies
+
+        //dynamic http request
+        for(j=1; j<3; j++){ //loops through all code for the various pages
+
+  
+    
+          const similarURL='https://api.themoviedb.org/3/movie/'+ movie_id +'/similar?api_key='+api_key+'&page='+j;
+        
+          
+          
+          
+          
+          $.getJSON(similarURL,function(result){
+
+      for(i=0; i<result.results.length;i++){
+
+         //Creating variable for rating value
+         var rating = Math.round(result.results[i].vote_average/2); //rounds up the rating devided by 2
+
+      
+         //output star rating
+         function star(rating) {
+             var starHTML = ''; //creates empty string where stars will be placed
+             var rate = parseInt(rating); //changes rating to int
+             var count = 0; //sets counter to zero
+             var max = 5; // maximum rating
+          
+             while(count < rate) { //outputs a star while the counter is less than the user rating
+               starHTML += '<i class="material-icons white">grade</i>';
+               count++;
+             }
+             while(max > rate) { //outputs dark stars for the rating the user didnt give
+                 starHTML += '<i class="material-icons gray">grade</i>';
+                 max--;
+               }
+          
+         
+             return starHTML;
+           };
+ 
+           var stars = star(rating);
+
+      $(".carouselbox.similar").append(
+ 
+        "      <div class='movieCard'>\
+        <img class='movieImg' src='https://image.tmdb.org/t/p/w500"+result.results[i].poster_path+"'/>\
+        <div class='overlayBlock'>\
+            <div class='divheart'>\
+                <img src='../img/other/heart_icon.svg'/>\
+            </div><!--divheart-->\
+            <div class='overlay'>\
+                <div class='hover-text'>\
+                    <h6>" + result.results[i].original_title +"</h6>\
+                    <h5>"+ stars +"</h5>\
+                    <a href = 'individualMovie.html?id="+ result.results[i].id +"'><div class='button1 movieButton'>Discover</div></a>\
+                    <div class='button2 movieButton'>Watch Later</div>\
+                </div><!--hoverText-->\
+             </div><!--overlay-->\
+        </div><!--overlayBlock-->\
+    </div><!--movieCard-->")
+
+      }
+    })
+  }
        
     });
 
-
+     
    
 
 
